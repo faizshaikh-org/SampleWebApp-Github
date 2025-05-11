08f23c07f03e38bfdeaade7b7f4774ad0c805e6b
Faiz Shaikh <Faiz.Shaikh@infovision.com>
2025-05-11 17:57:15 +0530

    Add project files.

---
+++ SampleWebApp-Github/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SampleWebApp_Github.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
